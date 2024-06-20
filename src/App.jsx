9b6d3f586b64185e393c8a90c7b32bfd49780a27
DaiNgo <daingo7211@gmail.com>
2024-06-20 21:38:34 +0700

    react 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
-import { Headline } from "./headline";
-import { ArticleContent } from "./article-content";
-import { Sidebar } from "./sidebar";
-import { Header } from "./header";
-import { Footer } from "./footer";
+import { ChangeColor } from "./modules/buoi-2/change-color";
 
 function App() {
   return (
-    <div>
-      {/* thẻ mở + thẻ đóng */}
-      <Header></Header>
-
-      {/* thẻ vừa mở vừa đóng */}
-      <Header />
-      <ArticleContent />
-      <Headline />
-      <Sidebar />
-
-      <Footer />
-    </div>
+    <>
+      <ChangeColor />
+    </>
   );
 }
 
